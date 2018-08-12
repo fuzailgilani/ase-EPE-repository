5d50d96b6dddbd5cd0b961fae73f105bda4c45c2
M Gomez <15865194+M-Gomez@users.noreply.github.com>
2018-08-12 02:42:43 -0500

    Turns out I broke the DB connection yesterday, mb ya'll.

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ var env = process.env.NODE_ENV || 'development';
 
 if(env === 'development') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/heroku_23fd6j4j';
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/EmpEval';
 } else if (env === 'test') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/heroku_23fd6j4j';
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/EmpEval';
 }
