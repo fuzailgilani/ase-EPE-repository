5d4335661ea993fee76e01d5a43db63efef31ba1
M Gomez <15865194+M-Gomez@users.noreply.github.com>
2018-08-11 23:09:52 -0500

    Removed Home Button from Approve Page

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ var env = process.env.NODE_ENV || 'development';
 
 if(env === 'development') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/EmpEval';
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/heroku_23fd6j4j';
 } else if (env === 'test') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/EmpEvalTest';
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/heroku_23fd6j4j';
 }
